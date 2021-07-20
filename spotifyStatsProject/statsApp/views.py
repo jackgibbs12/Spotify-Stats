9152fcf5c05a6253b843bb5f3dd911de7afcb19d
Jack Gibbs <jackgibbs66@icloud.com>
2021-07-20 16:02:45 +0100

    Fix layout, design improvements, modification to authentication

--- spotifyStatsProject/statsApp/views.py
+++ spotifyStatsProject/statsApp/views.py
@@ -1,49 +1,83 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 import spotipy
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from spotipy.oauth2 import SpotifyOAuth
 import os
 
 
 @csrf_exempt
 def index(request):
-    print(request.POST.get("time"))
+    """
+    View to authenticate the users Spotify account, get the time period (short, medium or long term) and
+    fetch the corresponding users top tracks.
+    """
+
+    # Import Spotify Client ID and Client Secret environment variables
     SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID']
     SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
+
+    # Define Scope and Redirect URI
     SPOTIPY_REDIRECT_URI = 'http://127.0.0.1:9090'
+
+    # Using 'user-top-read' scope as stated in Spotify's documentation:
+    # 'Read access to a user's top artists and tracks'
     SCOPE = 'user-top-read'
 
+    # Get the POST variable time to see whether user wants short, medium or long term tracks.
     if request.POST.get("time"):
         time = request.POST.get("time")
     else:
+        # If none selected, default to short term
         time = "short_term"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,
-                                                   redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
-    results = sp.current_user_top_tracks(limit=50, time_range=time)
 
-    def getTrackIDs(time_frame):
-        track_ids = []
-        for song in time_frame['items']:
-            track_ids.append(song['id'])
-        return track_ids
+    # Get authorisation from users Spotify account
+    auth = SpotifyOAuth(scope=SCOPE,
+                        client_id=SPOTIPY_CLIENT_ID,
+                        client_secret=SPOTIPY_CLIENT_SECRET,
+                        redirect_uri=SPOTIPY_REDIRECT_URI,
+                        cache_path=".spotifycache")
+
+    sp = spotipy.Spotify(auth_manager=auth)
+
+    # Get the users top 50 tracks.
+    results = sp.current_user_top_tracks(limit=50, time_range=time)
 
     track_ids = getTrackIDs(results)
 
-    def get_track_features(id):
-        meta = sp.track(id)
-        # meta
-        name = meta['name']
-        album = meta['album']['name']
-        artist = meta['album']['artists'][0]['name']
-        spotify_url = meta['external_urls']['spotify']
-        album_cover = meta['album']['images'][0]['url']
-        track_info = [name, album, artist, spotify_url, album_cover]
-        return track_info
-
+    # Iterate over each track ID and get the track info
     all_tracks = []
     for track_id in track_ids:
-        all_tracks.append(get_track_features(track_id))
-        context = {'tracks': all_tracks}
+
+        track_info = get_track_features(track_id, sp)
+        all_tracks.append(track_info)
+
+    context = {'tracks': all_tracks, 'time':time}
+    print(context)
 
     return render(request, 'index.html', context=context)
+
+
+def getTrackIDs(time_frame):
+    """
+    Function to iterate over users top songs and return a list of their IDs
+    """
+
+    track_ids = []
+    for song in time_frame['items']:
+        track_ids.append(song['id'])
+    return track_ids
+
+
+def get_track_features(id, sp):
+    """
+    Function which given a track ID will return the tracks info, including name, artist, URL and album cover
+    """
+
+    song = sp.track(id)
+    name = song['name']
+    album = song['album']['name']
+    artist = song['album']['artists'][0]['name']
+    spotify_url = song['external_urls']['spotify']
+    album_cover = song['album']['images'][0]['url']
+    track_info = [name, album, artist, spotify_url, album_cover]
+    return track_info
